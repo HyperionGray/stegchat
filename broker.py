0ea58ae216278f389110de2c04ee9053663e381b
Alex <acaceres@hyperiongray.com>
2018-08-27 16:47:47 -0400

    broker changes

--- broker.py
+++ broker.py
@@ -2,23 +2,31 @@ from flask import Flask, request, send_file
 from steganography.steganography import Steganography
 app = Flask(__name__)
 
+global chatlines
+chatlines = []
+
 @app.route("/blast", methods = ["GET", "POST"])
 def blast():
     if request.method  == "POST":
         message = request.form.get("message-ujwjejwjaq")
         print(message)
-        f = open("/tmp/msg", "w")
+        _chars = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
+        f = open("/tmp/msg" + _chars, "w")
         message = message.encode("utf-8").strip()
         f.write(message)
         f.close()
 
-        f = open("/tmp/msg")
+        f = open("/tmp/msg" + _chars)
+        chatlines.append(_chars)
         return f.read()
     
     if request.method == "GET":
-        f = open("/tmp/msg")
+        for chat in chatlines:
+            print(chat)
+
+        #f = open("/tmp/msg")
         #print(f.read())
-        return f.read()
+        #return f.read()
 
 @app.route("/img/ewflwkewhnuhfnuajfjn", methods = ["GET", "POST"])
 def image():
